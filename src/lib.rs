361a1e618520806bcf96c59dc31e54f416041985
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-04-22 16:37:03 +0900

    feat:hello wor;d

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
+pub mod db;
 pub mod model;
 pub mod router;
-pub mod db;
+use axum::{response::Html, routing::get, Router};
 use tokio;
 
+pub async fn run() {
+    let app = Router::new().route("/", get(index));
 
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
 
-pub async fn  run(){
-   println!("hello");
-
+async fn index() -> Html<&'static str> {
+    let html_content = include_str!("../index.html");
+    Html(html_content)
 }
